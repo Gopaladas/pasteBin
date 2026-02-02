9c8ddbb43d011ac3d4abd0c0a09b87cc89bc1c51
pavangopaldas <pavangopaldas@gmail.com>
2026-02-02 22:30:24 +0530

    done

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -3,15 +3,10 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectDB = async () => {
-  if (isConnected) {
-    return;
-  }
+  if (isConnected) return;
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     isConnected = true;
     console.log("MongoDB connected:", conn.connection.host);
   } catch (error) {

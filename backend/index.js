adbbb939105ca1d2dfa1fe0d572dd13dd11af106
pavangopaldas <pavangopaldas@gmail.com>
2026-02-02 22:15:10 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import { connectDB } from "./config/mongoDB.js";
 import binRoute from "./routers/pasteBinRouter.js";
 
 dotenv.config();
-
 const app = express();
 
-await connectDB();
+connectDB();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api", binRoute);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`successfully running at ${PORT}`);
+});
